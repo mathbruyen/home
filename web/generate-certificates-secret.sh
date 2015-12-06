e606f0da1f55c525e7b01a4fb7ab9ee99c1904ee
Mathieu Bruyen <code@mais-h.eu>
2015-12-06 18:07:03 +0100

    Script generating certificates secret
    Fixes #13

---
+++ web/generate-certificates-secret.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+cat > web-certificates.yaml << EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: webcertificates-v3
+type: Opaque
+data:
+  feeds.mais-h.eu.key: $(sudo openssl enc -A -base64 -in /etc/letsencrypt/live/feeds.mais-h.eu/privkey.pem)
+  feeds.mais-h.eu.cert: $(sudo openssl enc -A -base64 -in /etc/letsencrypt/live/feeds.mais-h.eu/fullchain.pem)
+  save.mais-h.eu.key: $(sudo openssl enc -A -base64 -in /etc/letsencrypt/live/save.mais-h.eu/privkey.pem)
+  save.mais-h.eu.cert: $(sudo openssl enc -A -base64 -in /etc/letsencrypt/live/save.mais-h.eu/fullchain.pem)
+EOF
