a9c5fae4a2b91f27ed80298ce841b7be7d0fae31
Mathieu Bruyen <code@mais-h.eu>
2016-01-20 08:43:24 +0100

    Disable HTTP for wallabag and ttrss, fixes #19

--- web/generate-web-config.sh
+++ web/generate-web-config.sh
@@ -4,7 +4,7 @@ cat > web-configuration.yaml << EOF
 apiVersion: v1
 kind: Secret
 metadata:
-  name: webconfiguration-v2
+  name: webconfiguration-v3
 type: Opaque
 data:
   ttrss.conf: $(openssl enc -A -base64 -in conf/ttrss.conf)
