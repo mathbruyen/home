ad2e96ad2434379d970429534e38e297c08f9da0
Mathieu Bruyen <code@mais-h.eu>
2016-01-20 09:22:11 +0100

    DCKEL website using https

--- web/generate-certificates-secret.sh
+++ web/generate-certificates-secret.sh
@@ -4,11 +4,13 @@ cat > web-certificates.yaml << EOF
 apiVersion: v1
 kind: Secret
 metadata:
-  name: webcertificates-v3
+  name: webcertificates-v5
 type: Opaque
 data:
   feeds.mais-h.eu.key: $(sudo openssl enc -A -base64 -in /etc/letsencrypt/live/feeds.mais-h.eu/privkey.pem)
   feeds.mais-h.eu.cert: $(sudo openssl enc -A -base64 -in /etc/letsencrypt/live/feeds.mais-h.eu/fullchain.pem)
   save.mais-h.eu.key: $(sudo openssl enc -A -base64 -in /etc/letsencrypt/live/save.mais-h.eu/privkey.pem)
   save.mais-h.eu.cert: $(sudo openssl enc -A -base64 -in /etc/letsencrypt/live/save.mais-h.eu/fullchain.pem)
+  dckel.mais-h.eu.key: $(sudo openssl enc -A -base64 -in /etc/letsencrypt/live/dckel.mais-h.eu/privkey.pem)
+  dckel.mais-h.eu.cert: $(sudo openssl enc -A -base64 -in /etc/letsencrypt/live/dckel.mais-h.eu/fullchain.pem)
 EOF
