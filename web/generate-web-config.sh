a23d9b96e7607262069300cb06634f3c56d7b7b5
Mathieu Bruyen <code@mais-h.eu>
2016-03-09 09:20:51 +0100

    Blog using kubernetes infrastructure

--- web/generate-web-config.sh
+++ web/generate-web-config.sh
@@ -4,10 +4,11 @@ cat > web-configuration.yaml << EOF
 apiVersion: v1
 kind: Secret
 metadata:
-  name: webconfiguration-v8
+  name: webconfiguration-v9
 type: Opaque
 data:
   ttrss.conf: $(openssl enc -A -base64 -in conf/ttrss.conf)
   wallabag.conf: $(openssl enc -A -base64 -in conf/wallabag.conf)
   dckel.conf: $(openssl enc -A -base64 -in conf/dckel.conf)
+  blog.conf: $(openssl enc -A -base64 -in conf/blog.conf)
 EOF
