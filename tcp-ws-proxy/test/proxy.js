54e6d93972fd63b896271ac75b63ec9ef3244a0b
Mathieu Bruyen <code@mais-h.eu>
2016-09-08 17:10:13 +0200

    Rationale connection closing

--- tcp-ws-proxy/test/proxy.js
+++ tcp-ws-proxy/test/proxy.js
@@ -30,61 +30,45 @@ function newTcpServer(listener) {
   });
 }
 
-function newEchoServer() {
-  return newTcpServer(c => c.pipe(c));
-}
-
-function expectEcho(proxyPort) {
+function newTcpClient(proxyPort, expected, listener) {
   return new Promise(function (resolve, reject) {
-    var received;
-    const client = net.connect({ port: proxyPort }, () => {
-      client.write('Hello world!\r\n');
-    });
-    client.on('data', (data) => {
-      received = data.toString();
-      client.end();
-    });
-    client.on('end', () => {
-      if (received === 'Hello world!\r\n') {
-        resolve();
-      } else {
-        reject(new Error('Invalid data received: ' + received));
+    let received = Buffer.alloc(0);
+    const earlyClose = () => reject(new Error('Server closed connection too early'));
+    const client = net.connect({ port: proxyPort }, () => listener(client));
+    client.on('data', data => {
+      received = Buffer.concat([received, data]);
+      if (received.toString() === expected) {
+        client.end();
+        client.removeListener('close', earlyClose);
+        client.on('close', resolve);
       }
     });
+    client.on('close', earlyClose);
+    client.on('error', reject);
   });
 }
 
 function newPushServer() {
   return newTcpServer(c => {
-    c.write('Hello push!\r\n');
+    c.write('Hello, push!');
     c.end();
   });
 }
 
 function expectPush(proxyPort) {
-  return new Promise(function (resolve, reject) {
-    var received;
-    const client = net.connect({ port: proxyPort });
-    client.on('data', (data) => {
-      received = data.toString();
-    });
-    client.on('end', () => {
-      if (received === 'Hello push!\r\n') {
-        resolve();
-      } else {
-        reject(new Error('Invalid data received: ' + received));
-      }
-    });
+  return newTcpClient(proxyPort, 'Hello, push!', client => {
   });
 }
 
 describe('Proxy', function() {
   describe('server', function() {
     it('should echo back', function() {
-      return newEchoServer().then(echo => {
+      return newTcpServer(c => c.pipe(c)).then(echo => {
         return createServer(0, `tcp://localhost:${echo.address().port}`, log).then(proxyServer => {
           return createProxy(0, 'ws://localhost:' + proxyServer.address().port, {}, log).then(proxyClient => {
-            return expectEcho(proxyClient.address().port).then(() => {
+            return newTcpClient(proxyClient.address().port, 'Hello, echo!', client => {
+              client.write('Hello, echo!');
+            }).then(() => {
               echo.close();
               proxyServer.close();
               proxyClient.close();
@@ -94,10 +78,29 @@ describe('Proxy', function() {
       });
     });
     it('should push data', function() {
-      return newPushServer().then(push => {
+      return newTcpServer(c => {
+        c.write('Hello, push!');
+        c.end();
+      }).then(push => {
         return createServer(0, `tcp://localhost:${push.address().port}`, log).then(proxyServer => {
           return createProxy(0, 'ws://localhost:' + proxyServer.address().port, {}, log).then(proxyClient => {
-            return expectPush(proxyClient.address().port).then(() => {
+            return newTcpClient(proxyClient.address().port, 'Hello, push!', client => {}).then(() => {
+              push.close();
+              proxyServer.close();
+              proxyClient.close();
+            });
+          });
+        });
+      });
+    });
+    it('should push large amount of data', function() {
+      return newTcpServer(c => {
+        c.write('Hello, large push!'.repeat(10000));
+        c.end();
+      }).then(push => {
+        return createServer(0, `tcp://localhost:${push.address().port}`, log).then(proxyServer => {
+          return createProxy(0, 'ws://localhost:' + proxyServer.address().port, {}, log).then(proxyClient => {
+            return newTcpClient(proxyClient.address().port, 'Hello, large push!'.repeat(10000), client => {}).then(() => {
               push.close();
               proxyServer.close();
               proxyClient.close();
